b5abf67b1c5b3163fe524963747ea4c0b43dbd02
Aaron Lenoir <lenoir.aaron@gmail.com>
2013-11-24 22:14:43 +0100

    build.cmd is not useful!

--- build.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
-msbuild "C:\Users\Public\Documents\Programming\github\flaclibsharp\FlacLibSharp\FlacLibSharp.sln" /p:Configuration=Debug /p:Platform="Any CPU"
