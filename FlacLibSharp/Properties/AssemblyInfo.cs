2edb27bbb4e7e62a80bad810014f2116d96b566a
Aaron Lenoir <AaronLenoir@users.noreply.github.com>
2016-07-12 21:37:35 +0200

    Upped the version to 2.0.0 and prepared Nuget spec for release.

--- FlacLibSharp/Properties/AssemblyInfo.cs
+++ FlacLibSharp/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription(".NET library to read FLAC metadata")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FlacLibSharp 1.5.0-beta")]
-[assembly: AssemblyCopyright("Copyright © Aaron Lenoir 2015")]
+[assembly: AssemblyProduct("FlacLibSharp 2.0.0")]
+[assembly: AssemblyCopyright("Copyright © Aaron Lenoir 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
